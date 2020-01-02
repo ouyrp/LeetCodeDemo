174c1906e834d8140c1c1ffd361f0e8e1b43877a
ouang <rp.ouyang001@bkjk.com>
2020-01-02 18:30:19 +0800

    Definition for singly-linked list

--- Leetcode-demo/src/main/java/TwoSum/TwoSum.java
+++ Leetcode-demo/src/main/java/TwoSum/TwoSum.java
@@ -8,8 +8,8 @@ public class TwoSum {
         int[] indexs = new int[2];
 
         HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
-        for(int i = 0; i < nums.length; i++) {
-            if(hash.containsKey(nums[i])) {
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.containsKey(nums[i])) {
                 indexs[0] = i;
                 indexs[1] = hash.get(nums[i]);
                 return indexs;
@@ -17,5 +17,38 @@ public class TwoSum {
             hash.put(target - nums[i], i);
         }
         return indexs;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode root = new ListNode(0);
+        ListNode cursor = root;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sumVal = l1Val + l2Val + carry;
+            carry = sumVal / 10;
+
+            ListNode sumNode = new ListNode(sumVal % 10);
+            cursor.next = sumNode;
+            cursor = sumNode;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return root.next;
     }
 }
